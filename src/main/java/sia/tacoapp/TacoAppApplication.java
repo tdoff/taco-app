1d4ccb3c578919c9a5645bcad913d508e03a3221
Danabekov <temirrrlan@gmail.com>
2021-10-31 14:35:48 +0600

    Initial commit

---
+++ src/main/java/sia/tacoapp/TacoAppApplication.java
@@ -0,0 +1,13 @@
+package sia.tacoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sia/tacoapp/TacoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.tacoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
